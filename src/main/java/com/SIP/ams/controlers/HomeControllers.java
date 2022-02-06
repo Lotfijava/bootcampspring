a9c9161638fcdfd83ddef4cd376adb1e0decdb0b
Lotfijava <lotfiguesmi23131916@gmail.com>
2022-02-06 15:52:27 +0100

    Tableau

--- src/main/java/com/SIP/ams/controlers/HomeControllers.java
+++ src/main/java/com/SIP/ams/controlers/HomeControllers.java
@@ -1,14 +1,23 @@
 package com.SIP.ams.controlers;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.ui.Model;
 @Controller
 public class HomeControllers {
 	
 @RequestMapping("/info")
-   public String info()
+public String info(Model model)
    {
+	
+	String formation="Fullstack" ;
+	model.addAttribute("workshop",formation);
+	
+	String liste[] = new String[]{"OCP","oca","Spring","Angular"};
+	ArrayList arlist = new ArrayList(Arrays.asList(liste));
+	model.addAttribute("tableau",arlist);
+	
 	   return "home/info" ;
    }
 
